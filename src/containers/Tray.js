7ab6525ad89ba95689d29f655fac89b4ffe96549
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-07-25 15:14:12 -0400

    Refactor

--- src/containers/Tray.js
+++ src/containers/Tray.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
-import { loadSongs } from '../actions/SongActions';
+import * as songActions from '../actions/SongActions';
 import { connect } from 'react-redux';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
 
 
 export class Tray extends Component {
-
+    
     render() {
         const songs = this.props.songs.map( (song,index) => {
             return <tr key={index}><td ><NavLink exact to={`/songs/${song.id}`}>
                 {song.title} - {song.artist}</NavLink></td></tr>
         })
+        songs.push(<tr key='-1'><td><NavLink exact to='/songs/new'>New Song...</NavLink></td></tr>)
         return (
-            
+            <div>
                 <Table striped bordered condensed >
                   <thead>
                       <tr><td>Songs</td></tr>
@@ -23,11 +24,12 @@ export class Tray extends Component {
                     {songs}
                     </tbody>
                 </Table>
-            
+                
+            </div>
         )
     }
     componentDidMount() {
-            this.props.loadSongs();
+            this.props.actions.loadSongs();
     }
 }
 
@@ -36,9 +38,9 @@ const mapStateToProps = (state) => {
         songs: state.songs
     }
 }
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-        loadSongs: loadSongs
-    }, dispatch)
+const mapDispatchToProps = (dispatch) => {  
+  return {
+    actions: bindActionCreators(songActions, dispatch)
+  };
 }
 export const ConnectedTray = connect(mapStateToProps,mapDispatchToProps)(Tray)
