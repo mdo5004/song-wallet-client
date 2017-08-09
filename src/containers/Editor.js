3dc5229aaa9ec92c293d9ecda0fbb8c26f170a5c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-09 17:25:26 -0400

    spacing

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -9,13 +9,13 @@ import { bindActionCreators } from 'redux';
 export class Editor extends Component {
     constructor(props){
         super(props);
-        
+
         this.state = {
             editing: false
         }
         this.handleChange = this.handleChange.bind(this);
     }
-    
+
     handleChange = (event) => {
         const song = parseSong(event.target.value);
         this.props.updateCurrentSong(song);
@@ -27,33 +27,38 @@ export class Editor extends Component {
         this.props.loadCurrentSong(songId);
     }
     componentDidMount(){
-        
+
         this.initializeComponent(this.props.match.params.songId);    
     }
     componentWillReceiveProps(nextProps){
-        if (nextProps.match.params.songId !== this.props.match.params.songId){
-            this.initializeComponent(nextProps.match.params.songId);
-        }
+    if (nextProps.match.params.songId !== this.props.match.params.songId){
+        this.initializeComponent(nextProps.match.params.songId);
     }
+}
     toggleEditing = () => {
-        const editing = this.state.editing;
-        this.setState({
-            editing: !editing,
-        })
-    }
-    render() {
-        
-        return (
-            <div>
-                
-                <div className='col-md-6'>
-            <textarea style={{height:'400px', width: '100%'}} onChange={this.handleChange} value={this.props.currentSong.content}></textarea><button onClick={this.saveCurrentSong}>Save</button>
-                </div><div className='col-md-6'>
-            <Display text={this.props.currentSong.content}/>
-                </div>
+    const editing = this.state.editing;
+    this.setState({
+        editing: !editing,
+    })
+}
+render() {
+
+    return (
+        <div>
+
+            <div className='col-md-6'>
+                <textarea 
+                   style={{height:'400px', width: '100%'}} 
+                   onChange={this.handleChange} 
+                   value={this.props.currentSong.content}></textarea>
+                <button onClick={this.saveCurrentSong}>Save</button>
             </div>
-        )
-    }
+            <div className='col-md-6'>
+                <Display text={this.props.currentSong.content}/>
+            </div>
+        </div>
+    )
+}
 }
 
 const mapDispatchToProps = (dispatch) => {
