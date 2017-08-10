0d63e9acb4e8a22f47b5224e7251b5870000c8bc
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-10 14:54:46 -0400

    editor toggle

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -21,7 +21,8 @@ export class Editor extends Component {
         this.props.updateCurrentSong(song);
     }
     saveCurrentSong = () => {
-        this.props.saveCurrentSong(this.props.match.params.songId,this.props.currentSong)
+        this.props.saveCurrentSong(this.props.match.params.songId, this.props.currentSong)
+        this.setState({ editing: false })
     }
     initializeComponent = (songId) => {
         this.props.loadCurrentSong(songId);
@@ -42,18 +43,17 @@ export class Editor extends Component {
     })
 }
 render() {
-
-    return (
-        <div>
-
-            <div className='col-md-6'>
+    const editor = this.state.editing ? (<div>
                 <textarea 
                    style={{height:'400px', width: '100%'}} 
                    onChange={this.handleChange} 
                    value={this.props.currentSong.content}></textarea>
                 <button onClick={this.saveCurrentSong}>Save</button>
-            </div>
-            <div className='col-md-6'>
+        </div>) : (<div><button value="Edit" onClick={ () => this.setState({ editing: true })}>Edit</button></div>)
+    return (
+        <div>
+            {editor}
+            <div>
                 <Display text={this.props.currentSong.content}/>
             </div>
         </div>
