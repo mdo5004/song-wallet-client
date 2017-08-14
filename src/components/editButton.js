ffc9d8e7c93b0d5f9a98047f2a9c99892c2938a4
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-14 11:02:24 -0400

    Edit/Save toggle button

---
+++ src/components/editButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const button = (props) => {
+    let buttonDisplay;
+    
+    if (props.editing){
+        buttonDisplay = <Button basic color='green' onClick={this.props.callback}>Edit</Button>;    
+    }
+    return (
+    
+        )
+}
--- src/components/setlist.js
+++ src/components/setlist.js
@@ -2,36 +2,60 @@ import React from 'react';
 import { Grid, Card, List, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-const Setlist = (props) => {
+export class Setlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false
+        }
+    }
+    editSetlist = () => {
+        const editing = this.state.editing
+        this.setState({
+            editing: !editing
+        })
+    }
 
-    const {name, songs, id} = props["setlist"];
-
-    const listOfSongs = songs.map( (song, index) => {
+    render() {
+        const {name, songs, id} = this.props["setlist"];
+        let listOfSongs, toggleButton;
+        if (this.state.editing) {
+            listOfSongs = songs.map( (song, index) => {
+                return (
+                    <List.Item key={index}>
+                        <List.Content>{song.title}</List.Content>
+                    </List.Item>
+                )})
+            toggleButton = <Button basic color='green' onClick={this.editSetlist}>Save</Button>
+        } else {
+            listOfSongs = songs.map( (song, index) => {
+                return (
+                    <List.Item key={index}>
+                        <List.Content as={NavLink} to={`/songs/${song.id}`}>{song.title}</List.Content>
+                    </List.Item>
+                )})
+           toggleButton = <Button basic color='yellow' onClick={this.editSetlist}>Edit</Button>
+        }
+        
         return (
-            <List.Item key={index}>
-                <List.Content>{song.title}</List.Content>
-            </List.Item>
+            <Grid.Column>
+                <Card >
+                    <Card.Content>
+                        <Card.Header>{name}</Card.Header>
+                    </Card.Content>    
+                    <Card.Content>
+                        {listOfSongs}
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                            {toggleButton}
+                            <Button basic color='red'>Delete</Button>
+                        </div>
+                    </Card.Content>
+                </Card>
+            </Grid.Column>
+
         )
-    })
-
-    return (
-        <Grid.Column>
-            <Card >
-                <Card.Content>
-                    <Card.Header>{name}</Card.Header>
-                </Card.Content>    
-                <Card.Content>
-                    {listOfSongs}
-                </Card.Content>
-                <Card.Content extra>
-                    <div className='ui two buttons'>
-                        <Button basic color='green' as={NavLink} to={`/setlists/${id}/edit`}>Edit</Button>
-                        <Button basic color='red'>Delete</Button>
-                    </div>
-                </Card.Content>
-            </Card>
-        </Grid.Column>
-
-    )
+    }
 }
 export default Setlist;
